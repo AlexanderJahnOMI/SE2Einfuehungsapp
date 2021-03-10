61e8c296e753cc061d899271df1bd2370adcc1ce
Alex <alexanderja@edu.aau.at>
2021-03-10 09:33:11 +0100

    added server communication and multithreading

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -8,6 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,42 +20,74 @@ public class MainActivity extends AppCompatActivity {
     private EditText matrEingabefeld;
     private TextView ergebnisAusgeben;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Die Variablen werden mit der ID ihres jeweiligen Buttons/Textfeldes belegt.
         abschicken = (Button) findViewById(R.id.sendServerButton);
         matrEingabefeld = (EditText) findViewById(R.id.matrEingabefeld);
         ergebnisAusgeben = (TextView) findViewById(R.id.ergebnisAusgeben);
 
-        //Dem OnClickListener muss gesagt werden, dass er auf den Button achten soll
         abschicken.setOnClickListener(this::onClick);
-
-
-
     }
 
 
-    // onClick: Wird aufgerufen, wenn auf einen Button geklickt wird.
-    public void onClick(View view) {
-        if (view == abschicken) {
+    public void onClick(View v) {
+        SubActivity subActivity = new SubActivity();
 
-            // Der Text muss in einen Integer geändert werden.
-            Integer matrikelNummer;
-            matrikelNummer = Integer.valueOf(matrEingabefeld.getText().toString());
+        Thread thread = new Thread(subActivity);
 
-            matrikelNummer = matrikelNummer+5;
+        thread.start();
 
-            // Ergebnis der Berechnung wird ausgegeben.
-            ergebnisAusgeben.setText(matrikelNummer.toString());
+        try {
+            thread.join();
+        }catch (InterruptedException ie) {
+            ergebnisAusgeben.setText("Fehler");
+        }
+
+        ergebnisAusgeben.setText(subActivity.getMatrNr());
+    }
+
+
+
+
+    public class SubActivity extends Thread {
+
+        private String matrNr = matrEingabefeld.getText().toString();
+
+        public void run(){
+            try {
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                outToServer.writeBytes(matrNr+ '\n');
+
+                setMatrNr(inFromServer.readLine());
+
+                clientSocket.close();
+
+            } catch (Exception e){
+                ergebnisAusgeben.setText(e.toString());
+            }
 
         }
 
+        public String getMatrNr (){
+            return this.matrNr;
+        }
+
+        public void setMatrNr(String matrNr){
+            this.matrNr = matrNr;
+        }
+
     }
+
+
+
 
 
 
