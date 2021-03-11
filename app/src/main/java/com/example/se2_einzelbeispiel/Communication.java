6652f8cc9b03680b3b5e2d1d2df85b546b7a641e
Alex <alexanderja@edu.aau.at>
2021-03-11 10:58:20 +0100

    method calculatedAscii has been added and some code smells have been eliminated

--- app/src/main/java/com/example/se2_einzelbeispiel/Communication.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/Communication.java
@@ -10,44 +10,56 @@ import java.net.Socket;
 
 public class Communication extends Thread {
 
-
     private String message;
     private String host;
     private int port;
 
+    /**
+     * constructor
+     * @param message is the message which get send to the server and after the server answers its the answer from server
+     * @param host server host name
+     * @param port which port should be used
+     */
     public Communication(String message, String host, int port){
-            this.message = message;
-            this.host = host;
-            this.port = port;
+        this.message = message;
+        this.host = host;
+        this.port = port;
     }
 
-        public void run(){
-            try {
-                Socket clientSocket = new Socket(host, port);
+    /**
+     * gets started when a method creates an Communication object and uses method start()
+     * communicates with the server
+     */
+    public void run(){
+        try {
+            Socket clientSocket = new Socket(host, port);
 
-                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                outToServer.writeBytes(this.message + '\n');
+            outToServer.writeBytes(this.message + '\n');
 
-                this.message = (inFromServer.readLine());
+            this.message = (inFromServer.readLine());
 
-                clientSocket.close();
+            clientSocket.close();
 
-            } catch (Exception e){
-                this.message = e.toString();
-            }
-
+        } catch (Exception e){
+            this.message = e.toString();
         }
 
-        public String getMessage (){
-            return this.message;
-        }
+    }
 
-        private void setMessage(String message){
-            this.message = message;
-        }
+    /**
+     * getter and setter to get and set message
+     * getMessage should be used to get the answer from server
+     */
+    public String getMessage (){
+        return this.message;
+    }
 
+    private void setMessage(String message){
+        this.message = message;
+    }
 
 }
--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -10,26 +10,55 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button abschicken;
+    /**
+     * sendServerButton is needed to start method calculateStudyBeginning
+     * asciiButton is needed to start method calculateAscii
+     * matrEingabefeld gets the value which is tipped in by the user. this value is needed in method calculateStuyBeginning as well in calculateAscii. values can only be numberic
+     * ergebnisAusgeben shows the user the results of the calculations
+     */
+    private Button sendServerButton;
+    private Button asciiButton;
     private EditText matrEingabefeld;
     private TextView ergebnisAusgeben;
 
+    /**
+     * starts when the app gets started and waits until a button was clicked
+     * when a button gets clicked onClick gets called
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        abschicken = (Button) findViewById(R.id.sendServerButton);
+        sendServerButton = (Button) findViewById(R.id.sendServerButton);
+        asciiButton = (Button) findViewById(R.id.asciiButton);
         matrEingabefeld = (EditText) findViewById(R.id.matrEingabefeld);
         ergebnisAusgeben = (TextView) findViewById(R.id.ergebnisAusgeben);
 
-        abschicken.setOnClickListener(this::onClick);
+        sendServerButton.setOnClickListener(this);
+        asciiButton.setOnClickListener(this);
     }
 
-
+    /**
+     * finds out which button was pressed and starts the correct method
+     * onClick is called by onCreate
+     * @param view saves the button which was clicked
+     */
     public void onClick(View view) {
+        if (view == sendServerButton){
+            calculateStudyBeginning();
+        } else if (view == asciiButton){
+            calculateAscii();
+        }
+    }
+
+    /**
+     * starts the connection to the server and waits until an answer arrives
+     * the answer shows up on the app
+     */
+    public void calculateStudyBeginning(){
         Communication subActivity = new Communication(matrEingabefeld.getText().toString(), "se2-isys.aau.at", 53212);
 
         subActivity.start();
@@ -39,16 +68,30 @@ public class MainActivity extends AppCompatActivity {
 
             ergebnisAusgeben.setText(subActivity.getMessage());
         }catch (InterruptedException ie) {
-            ergebnisAusgeben.setText("Something happend.");
+            ergebnisAusgeben.setText("Something happened.");
         }
-
     }
 
+    /**
+     * replaces every second number of matrEingabefeld with an ASCII charakter and shows up the result on the app
+     */
+    public void calculateAscii(){
+        char [] arr = matrEingabefeld.getText().toString().toCharArray();
+        String buildingItUpAgain = "";
 
+        for (int i = 0; i < arr.length; i++){
+            if ((i+1) % 2 == 0){
+                int charValue = 122;
+                if (arr[i] != '0'){
+                    charValue = 96 + Integer.parseInt(String.valueOf(arr[i]));
+                }
+                buildingItUpAgain += (char) charValue;
+            } else {
+                buildingItUpAgain += arr[i];
+            }
+        }
 
-
-
-
-
+        ergebnisAusgeben.setText(buildingItUpAgain);
+    }
 
 }
